4a9106879787410c47dc144669839f8a598679c1
robinmagot <magot.robin@gmail.com>
2021-10-30 14:47:09 +0200

    defvalid+defgeneralscore

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :scores
   has_many :indicators, through: :scores
+
+  #def general_score
+    #score_count = content.valid?.count / content.count
+  #end
 end
